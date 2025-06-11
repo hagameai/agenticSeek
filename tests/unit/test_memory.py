99aeab3415caa00ff2a6cc41bf480903a0225802
elicc <hagame1988@163.com>
2025-06-11 21:22:21 +0800

    test: add unit tests for memory module

--- tests/unit/test_memory.py
+++ tests/unit/test_memory.py
@@ -1,47 +1,38 @@
-import pytest
+import unittest
 from sources.memory import Memory
 
+class TestMemory(unittest.TestCase):
+    def setUp(self):
+        # This method will run before each test.
+        self.memory = Memory()  # Assuming Memory is a class
 
-def test_memory_initialization():
-    """
-    Test the initialization of the Memory class.
-    """
-    memory = Memory()
-    assert memory is not None
+    def test_initialize_memory(self):
+        # Test if the memory initializes correctly.
+        self.assertIsNotNone(self.memory)
+        self.assertEqual(self.memory.data, {})
 
+    def test_add_memory(self):
+        # Test adding an item to memory.
+        self.memory.add('key1', 'value1')
+        self.assertEqual(self.memory.get('key1'), 'value1')
 
-def test_memory_store_and_retrieve():
-    """
-    Test storing and retrieving a value in memory.
-    """
-    memory = Memory()
-    memory.store('key1', 'value1')
-    assert memory.retrieve('key1') == 'value1'
+    def test_get_memory(self):
+        # Test getting an item from memory.
+        self.memory.add('key2', 'value2')
+        self.assertEqual(self.memory.get('key2'), 'value2')
+        self.assertIsNone(self.memory.get('nonexistent_key'))
 
+    def test_remove_memory(self):
+        # Test removing an item from memory.
+        self.memory.add('key3', 'value3')
+        self.memory.remove('key3')
+        self.assertIsNone(self.memory.get('key3'))
 
-def test_memory_retrieve_nonexistent_key():
-    """
-    Test retrieving a value for a nonexistent key returns None.
-    """
-    memory = Memory()
-    assert memory.retrieve('nonexistent_key') is None
+    def test_clear_memory(self):
+        # Test clearing all memory.
+        self.memory.add('key4', 'value4')
+        self.memory.clear()
+        self.assertEqual(self.memory.data, {})
 
-
-def test_memory_update_value():
-    """
-    Test updating a stored value in memory.
-    """
-    memory = Memory()
-    memory.store('key1', 'value1')
-    memory.store('key1', 'value2')
-    assert memory.retrieve('key1') == 'value2'
-
-
-def test_memory_clear():
-    """
-    Test clearing all stored values in memory.
-    """
-    memory = Memory()
-    memory.store('key1', 'value1')
-    memory.clear()
-    assert memory.retrieve('key1') is None
+if __name__ == '__main__':
+    unittest.main()
